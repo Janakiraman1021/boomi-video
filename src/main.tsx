fb7a57120b855fe9e85f5c4db633c42d7bf3dd0a
JANAKIRAMAN <janakiramankeerthivelan1021@gmail.com>
2024-03-29 23:35:42 +0530

    first commit

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot as CreateRoot } from "react-dom/client";
-import App from "./App.tsx";
-
-CreateRoot(document.getElementById("root") as HTMLElement).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
